99ea1236fb3e1c15ff8d6ef4ab2f9222cab9f1b5
Styer, Adam <88atti@gmail.com>
2017-11-08 15:36:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -27,34 +27,6 @@ while True:
             print("Thank you for taking the time to use your watch!")
             break
 
-    elif choice == "3":
-        start = input("Press enter to start and stop the stopwatch.")
-        startTime = time.time()
-        print("0")
-        secs = 1
-        goOn = True
-        keyboard.release('enter')
-        while goOn is True:
-            currentTime = time.time()
-            endTime = currentTime + 1
-            while (time.time() < endTime) & (goOn is True):
-                try:
-                    if keyboard.is_pressed('enter'):
-                        goOn = False
-                        totalendTime = time.time()
-                except:
-                    pass
-            if goOn is True:
-                print(secs)
-                secs+=1
-        timeElapsed = totalendTime - startTime
-        print("Time: " + str(timeElapsed)[:4] + "s")
-        enterkill = input(""),
-        yorn = input("Would you like to continue using your watch? (Enter yes or no)")
-        if (yorn.lower() == "no"):
-            print("Thank you for taking the time to use your watch!")
-            break
-
     elif choice == "4":
         while True:
             timerlengthS = input("Enter the amount of seconds you want your timer to last for.")
